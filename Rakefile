86a5695e8896f48ea067db3f22023acf004ba43c
Ellie <elliewem@gmail.com>
2018-01-25 16:59:08 +0000

    Add sign up function

--- Rakefile
+++ Rakefile
@@ -1,11 +1,14 @@
 require 'data_mapper'
+require_relative './app/app.rb'
 
 task default: %w[upgrade]
 
 task :upgrade do
   DataMapper.auto_upgrade!
+  p "Datamapper has auto upgraded"
 end
 
 task :migrate do
   DataMapper.auto_migrate!
+  p "Datamapper has auto migrated"
 end
--- app/app.rb
+++ app/app.rb
@@ -3,14 +3,17 @@ require 'sinatra/base'
 require_relative './data_mapper_setup'
 require_relative './models/link.rb'
 require_relative './models/tag.rb'
+require_relative './models/user.rb'
 
 class BookMarkManager < Sinatra::Base
+  enable :sessions
 
   get '/' do
     erb(:main)
   end
 
   get '/links' do
+    @user = User.get(session['id'])
     @links = Link.all
     erb(:links)
   end
@@ -30,6 +33,16 @@ class BookMarkManager < Sinatra::Base
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb(:links)
+  end
+
+  get '/sign_up' do
+    erb(:sign_up)
+  end
+
+  post '/sign_up' do
+    user = User.create(email: params[:email], password: params[:password])
+    session['id'] = user.id
+    redirect to('/links')
   end
 
 end
